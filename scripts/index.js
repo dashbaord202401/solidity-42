e043b6c6cb165d186c8286f32d4590c3f3ee42ab
Barthalomena <93641547+Barthalomena26@users.noreply.github.com>
2022-05-16 22:38:10 +0530

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+async function main() {
+  // const accounts = await ethers.provider.listAccounts();
+  //   console.log(accounts);
+  const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const Box = await ethers.getContractFactory("Box");
+  const box = await Box.attach(address);
+  await box.store(23);
+  const value = await box.retrieve();
+  console.log("Box value is", value.toString());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
